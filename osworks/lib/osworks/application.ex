ecbb6a98c67a534263fa90f51adf291d7cf7dddb
phravins <phravintreuno@gmail.com>
2026-04-05 23:49:55 +0530

    Add lib/osworks/application.ex — OTP application supervisor

---
+++ osworks/lib/osworks/application.ex
@@ -0,0 +1,34 @@
+defmodule Osworks.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      OsworksWeb.Telemetry,
+      Osworks.Repo,
+      {DNSCluster, query: Application.get_env(:osworks, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Osworks.PubSub},
+      # Start a worker by calling: Osworks.Worker.start_link(arg)
+      # {Osworks.Worker, arg},
+      # Start to serve requests, typically the last entry
+      OsworksWeb.Endpoint
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Osworks.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    OsworksWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
